7155b0edf223b93b6f585c6eeebed1c3ddc6f0a8
queue <phyque.zz@gmail.com>
2023-09-24 01:12:45 +0800

    use-package

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-(eval-when-compile 
- (require 'use-package))
-
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 ;; BASIC configuration
@@ -41,54 +38,10 @@
 (global-set-key (kbd "M-n") 'next-ten-lines)
 (global-set-key (kbd "M-p") 'previous-ten-lines)
 
+;;; ABOUNT Package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-
-(when (display-graphic-p) (load-theme 'dracula t))
-(when (display-graphic-p) (require 'all-the-icons))
-(add-hook 'after-init-hook 'global-company-mode)
-(require 'company-box)
-(add-hook 'company-mode-hook 'company-box-mode)
-
-;; IVY
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-(setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-(good-scroll-mode 1)
-(global-set-key [next] #'good-scroll-up-full-screen)
-(global-set-key [prior] #'good-scroll-down-full-screen)
-
-(setq chatgpt-shell-openai-key "i have no key here")
-
-(google-this-mode 1)
-
-(require 'google-translate)
-(require 'google-translate-default-ui)
-(global-set-key "\C-ct" 'google-translate-at-point)
-(global-set-key "\C-cT" 'google-translate-query-translate)
-(setq google-translate-default-source-language "en")
-(setq google-translate-default-target-language "zh-CN")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -105,8 +58,96 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;; Package setup
+(eval-when-compile 
+  (require 'use-package))
+
+(use-package tree-sitter-indent
+  :ensure t)
+(use-package tree-sitter
+  :ensure t)
+(use-package magit
+  :ensure t)
+(use-package pdf-tools
+  :ensure t)
+
+(use-package google-translate
+  :ensure t
+  :init
+  (setq google-translate-default-source-language "en")
+  (setq google-translate-default-target-language "zh-CN")
+  (setq google-translate-output-destination 'popup)
+  :bind (("\C-ct" . 'google-translate-at-point)
+         ("\C-cT" . 'google-translate-query-translate))
+  :config
+  (require 'google-translate-default-ui))
+
+(use-package google-this
+  :ensure t
+  :config
+  (google-this-mode 1))
+
+(use-package good-scroll
+  :ensure t
+  :bind (([next] . #'good-scroll-up-full-screen)
+         ([prior] . #'good-scroll-down-full-screen))
+  :config
+  (good-scroll-mode 1))
+
+(use-package swiper
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+
+(use-package ivy
+  :ensure t
+  :init
+  (ivy-mode 1)
+  (counsel-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  ;; enable this if you want `swiper' to use it
+  (setq search-default-mode #'char-fold-to-regexp)
+  :bind (("C-s" . 'swiper)
+         ("C-c C-r" . 'ivy-resume)
+         ("<f6>" . 'ivy-resume)
+         ("M-x" . 'counsel-M-x)
+         ("C-x C-f" . 'counsel-find-file)
+         ("<f1> f" . 'counsel-describe-function)
+         ("<f1> v" . 'counsel-describe-variable)
+         ("<f1> o" . 'counsel-describe-symbol)
+         ("<f1> l" . 'counsel-find-library)
+         ("<f2> i" . 'counsel-info-lookup-symbol)
+         ("<f2> u" . 'counsel-unicode-char)
+         ("C-c g" . 'counsel-git)
+         ("C-c j" . 'counsel-git-grep)
+         ("C-c k" . 'counsel-ag)
+         ("C-x l" . 'counsel-locate)
+         ("C-S-o" . 'counsel-rhythmbox)
+         :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package company
+  :ensure t
+  :hook (('after-init-hook . 'global-company-mode)))
+
+(use-package company-box
+  :ensure t
+  :hook (('company-mode-hook . 'company-box-mode)))
+
+(use-package all-the-icons
+  :ensure t
+  :config
+  (when (display-graphic-p) (require 'all-the-icons)))
+
+(use-package dracula-theme
+  :ensure t
+  :config
+  (when (display-graphic-p) (load-theme 'dracula t)))
+
 ;;; org-mode
-;(require 'init-org)
 (defvar *dir-of-org* "~/Documents/org/")
 (setq org-directory (file-truename *dir-of-org*))
 
