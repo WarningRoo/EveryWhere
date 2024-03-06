80a43e2e305f089e8af9dc80a1a18bb26359d0a6
queue <phyque.zz@gmail.com>
2024-03-06 22:17:15 +0800

    refresh

--- .vimrc
+++ .vimrc
@@ -3,7 +3,7 @@
 " 1. Change to the project's root directory and 'mkdir -p .cache'
 " 2. Open vim directly in the project's root directory and execute '<C-@>' to
 "    generate the index files.
-" 3. That's it.
+" 3. That's all.
 " ATTENTION:
 " a. If you open Vim in a subdirectory below the project's root directory.
 "    Don't worry; the following code will automatically search upwards to find
@@ -45,8 +45,8 @@ Plug 'ycm-core/YouCompleteMe', { 'do': './install.py' }
 
 " +++++++++++++++++++++++++++++++++++++++++++
 " LSP
-Plug 'prabirshrestha/vim-lsp'
-Plug 'mattn/vim-lsp-settings'
+"Plug 'prabirshrestha/vim-lsp'
+"Plug 'mattn/vim-lsp-settings'
 " LSP
 " +++++++++++++++++++++++++++++++++++++++++++
 
@@ -341,42 +341,42 @@ endif
 "				\ })
 "endif
 "
-if executable('clangd')
-    au User lsp_setup call lsp#register_server({
-        \ 'name': 'clangd',
-        \ 'cmd': {server_info->['clangd', '-background-index']},
-        \ 'whitelist': ['c', 'cpp', 'objc', 'objcpp'],
-        \ })
-endif
+"if executable('clangd')
+"    au User lsp_setup call lsp#register_server({
+"        \ 'name': 'clangd',
+"        \ 'cmd': {server_info->['clangd', '-background-index']},
+"        \ 'whitelist': ['c', 'cpp', 'objc', 'objcpp'],
+"        \ })
+"endif
 
-function! s:on_lsp_buffer_enabled() abort
-	setlocal omnifunc=lsp#complete
-	setlocal signcolumn=yes
-	if exists('+tagfunc') | setlocal tagfunc=lsp#tagfunc | endif
-	nmap <buffer> <leader>gd <plug>(lsp-definition)
-	nmap <buffer> <leader>gs <plug>(lsp-document-symbol-search)
-	nmap <buffer> <leader>gS <plug>(lsp-workspace-symbol-search)
-	nmap <buffer> <leader>gr <plug>(lsp-references)
-	nmap <buffer> <leader>gi <plug>(lsp-implementation)
-	nmap <buffer> <leader>gt <plug>(lsp-type-definition)
-	nmap <buffer> <leader>rn <plug>(lsp-rename)
-	nmap <buffer> [g <plug>(lsp-previous-diagnostic)
-	nmap <buffer> ]g <plug>(lsp-next-diagnostic)
-	nmap <buffer> K <plug>(lsp-hover)
+"function! s:on_lsp_buffer_enabled() abort
+"	setlocal omnifunc=lsp#complete
+"	setlocal signcolumn=yes
+"	if exists('+tagfunc') | setlocal tagfunc=lsp#tagfunc | endif
+"	nmap <buffer> <leader>gd <plug>(lsp-definition)
+"	nmap <buffer> <leader>gs <plug>(lsp-document-symbol-search)
+"	nmap <buffer> <leader>gS <plug>(lsp-workspace-symbol-search)
+"	nmap <buffer> <leader>gr <plug>(lsp-references)
+"	nmap <buffer> <leader>gi <plug>(lsp-implementation)
+"	nmap <buffer> <leader>gt <plug>(lsp-type-definition)
+"	nmap <buffer> <leader>rn <plug>(lsp-rename)
+"	nmap <buffer> [g <plug>(lsp-previous-diagnostic)
+"	nmap <buffer> ]g <plug>(lsp-next-diagnostic)
+"	nmap <buffer> K <plug>(lsp-hover)
 "	nnoremap <buffer> <expr><c-f> lsp#scroll(+4)
 "	nnoremap <buffer> <expr><c-d> lsp#scroll(-4)
 
-	let g:lsp_format_sync_timeout = 1000
-	autocmd! BufWritePre *.rs,*.go call execute('LspDocumentFormatSync')
+"	let g:lsp_format_sync_timeout = 1000
+"	autocmd! BufWritePre *.rs,*.go call execute('LspDocumentFormatSync')
 
 	" refer to doc to add more commands
-endfunction
+"endfunction
 
-augroup lsp_install
-	au!
+"augroup lsp_install
+"	au!
 	" call s:on_lsp_buffer_enabled only for languages that has the server registered.
-	autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
-augroup END
+"	autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled()
+"augroup END
 
 "-------------------------------------------------------------------------------------------------
 
--- init.el
+++ init.el
@@ -51,18 +51,24 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(global-display-line-numbers-mode t)
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-path-style 'relative)
  '(package-selected-packages
-   '(dashboard amx molokai-theme tree-sitter-indent tree-sitter org-roam use-package google-translate google-this magit company-box good-scroll counsel swiper ivy company all-the-icons dracula-theme cmake-mode)))
+   '(treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-projectile treemacs lsp-ivy lsp-ui counsel-projectile projectile lsp-mode highlight-symbol mwim dashboard amx molokai-theme tree-sitter-indent tree-sitter org-roam use-package google-translate google-this magit company-box good-scroll counsel swiper ivy company all-the-icons dracula-theme cmake-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "FiraCode Nerd Font Mono" :foundry "CTDB" :slant normal :weight regular :height 203 :width normal)))))
+ '(default ((t (:family "FiraCode Nerd Font Mono" :foundry "CTDB" :slant normal :weight regular :height 110 :width normal))))
+ '(org-level-1 ((t (:inherit outline-1 :extend nil :height 2.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.6))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2)))))
 
 ;;; Package setup
-(eval-when-compile 
+(eval-when-compile
   (require 'use-package))
 
 (use-package dashboard
@@ -78,6 +84,25 @@
 (use-package amx
   :ensure t
   :init (amx-mode))
+
+(use-package mwim
+  :ensure t
+  :bind
+  ("C-a" . mwim-beginning-of-code-or-line)
+  ("C-e" . mwim-end-of-code-or-line))
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode))
+
+(use-package highlight-symbol
+  :ensure t
+  :init (highlight-symbol-mode)
+  :bind ("<f9>" . highlight-symbol))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package tree-sitter-indent
   :ensure t)
@@ -148,11 +173,14 @@
 
 (use-package company
   :ensure t
-  :hook (('after-init-hook . 'global-company-mode)))
+  :init (global-company-mode))
+;  :hook (after-init-hook . global-company-mode))
+
+;(add-hook 'after-init-hook 'global-company-mode)
 
 (use-package company-box
   :ensure t
-  :hook (('company-mode-hook . 'company-box-mode)))
+  :hook ((company-mode-hook . company-box-mode)))
 
 (use-package all-the-icons
   :ensure t
@@ -169,6 +197,168 @@
 ;(when (display-graphic-p) (load-theme 'dracula t))
 (when (display-graphic-p) (load-theme 'molokai t))
 (load-theme 'molokai t)
+
+;;;LSP
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (c-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+;; if you are helm user
+;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy
+  :ensure t
+  :commands lsp-ivy-workspace-symbol)
+;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+;(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
+(use-package counsel-projectile
+  :ensure t
+  :after (projectile)
+  :init (counsel-projectile-mode))
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                2000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-project-follow-into-home        nil
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (when treemacs-python-executable
+      (treemacs-git-commit-diff-mode t))
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+;(use-package treemacs-evil
+;  :after (treemacs evil)
+;  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+;(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+;  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+;  :ensure t
+;  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
+  :after (treemacs)
+  :ensure t
+  :config (treemacs-set-scope-type 'Tabs))
+
+
+
+
+
+
 ;;; org-mode
 (defvar *dir-of-org* "~/Documents/org/")
 (setq org-directory (file-truename *dir-of-org*))
@@ -189,19 +379,17 @@
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
-  (require 'org-roam-protocol)
-
-  (org-toggle-inline-images)
+  (require 'org-roam-protocol))
+;  (org-toggle-inline-images)
   ;; Support source code highlight
-  (require 'ob-shell)
-  (require 'ob-c)
-  (require 'ob-lisp)
-  (require 'ob-lua)
-  (require 'ob-elisp))
+;  (require 'ob-shell)
+;  (require 'ob-lisp)
+;  (require 'ob-lua)
+;  (require 'ob-elisp))
   ;; active Babel languages
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((R . t)
-     (emacs-lisp . t)
-     (lisp . t)))
+;  (org-babel-do-load-languages
+;   'org-babel-load-languages
+;   '((R . t)
+;     (emacs-lisp . t)
+;     (lisp . t)))
 (provide 'init)
